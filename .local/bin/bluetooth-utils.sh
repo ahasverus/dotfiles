f3e28361cad6e792b41fc14c40d8b930f5cb5c7e
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-01-26 18:24:39 +0100

    Add bluetooth devices

--- .local/bin/bluetooth-utils.sh
+++ .local/bin/bluetooth-utils.sh
@@ -12,6 +12,8 @@ bt () {
         "black") device='F8:4E:17:73:78:4F';;
         "white") device='F8:4E:17:E6:ED:6D';;
         "bose") device='60:AB:D2:27:D1:76';;
+        "quiet")  device='60:AB:D2:B2:B6:D3';;
+        "mxkeys") device='D4:F5:72:09:48:51';;
     esac
 
     bluetoothctl $action $device
