67737ef61d4162b09820b0c43f240632be8b3cb7
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-01-29 12:38:28 +0100

    Modify module margins

--- .config/polybar/scripts/bluetooth.sh
+++ .config/polybar/scripts/bluetooth.sh
@@ -2,9 +2,16 @@
 
 blue_status=$(bluetooth | grep -c "on")
 
-if [[ blue_status -eq 1 ]]
+if [[ blue_status -eq 0 ]]
 then
-    echo "%{F#d8dee9}󰂯%{T4} On %{T-}%{F-}"
+    echo "%{F#555555}󰂲%{T4} Down%{T-}%{F-}"
 else
-    echo "%{F#555555}󰂲%{T4} Off %{T-}%{F-}"
+    sleep 1
+    device=$(bluetoothctl devices Connected | cut -d ' ' -f 3-)
+    if [[ "$device" = "" ]]
+    then
+        echo "%{F#d8dee9}󰂯%{T4} ────%{T-}%{F-}"
+    else
+        echo "%{F#d8dee9}󰂯%{T4} $device%{T-}%{F-}"
+    fi
 fi
